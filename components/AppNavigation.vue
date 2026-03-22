28ab3c08749c6543e07b25e7bbf0909b036d47b3
sbansss <yiftyifiyift@gmail.com>
2026-03-22 21:23:13 +0300

    Create AppNavigation.vue with navigation bar and menu items

---
+++ components/AppNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="glitch-hover">
+    <div class="logo">My Logo</div>
+    <ul class="menu">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppNavigation',
+};
+</script>
+
+<style scoped>
+.glitch-hover {
+  position: relative;
+  display: inline-block;
+}
+
+.logo {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.menu {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 20px;
+}
+
+.menu li {
+  position: relative;
+}
+
+.menu li a {
+  text-decoration: none;
+  color: #000;
+}
+</style>
