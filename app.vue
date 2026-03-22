dc1da185fdbd73dd070c36af2558b49aa90f0c21
sbansss <yiftyifiyift@gmail.com>
2026-03-22 21:23:17 +0300

    Create app.vue with Nuxt layout and main app structure

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style>
+/* Add any global styles here */
+</style>
